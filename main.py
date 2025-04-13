044f8e03013bc9fb570e07a0b02648485d1bd709
Alireza <alireza.naziri@gmail.com>
2025-04-13 09:44:22 +0000

    marketcap above 10B

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ from utils import upload_to_hf_dataset, download_from_hf_dataset, load_hf_datase
 # current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 current_datetime = datetime.now().strftime("%Y-%m-%d")
 
-#Output folder for option chains
-outputfolder = 'optionchains'
+# Output folder for option chains
+outputfolder = "optionchains"
 
 # Create the gurufocus folder if it doesn't exist
 if not os.path.exists(outputfolder):
@@ -27,30 +27,30 @@ if not os.path.exists(outputfolder):
 load_dotenv()
 
 # Get the name of the HuggingFace dataset for TradingView to read from
-dataset_name_TradingView_input = os.getenv('dataset_name_TradingView_input')
+dataset_name_TradingView_input = os.getenv("dataset_name_TradingView_input")
 
 # Get the name of the HuggingFace dataset for YfOptions to export
-dataset_name_YfOptions_output = os.getenv('dataset_name_YfOptions_output')
+dataset_name_YfOptions_output = os.getenv("dataset_name_YfOptions_output")
 
 # Get the Hugging Face API token from the environment; either set in .env file or in the environment directly in GitHub
-HF_TOKEN_YfOptions = os.getenv('HF_TOKEN_YfOptions')
+HF_TOKEN_YfOptions = os.getenv("HF_TOKEN_YfOptions")
 
-#Load lastest TradingView DataSet from HuggingFace Dataset which is always america.csv
+# Load lastest TradingView DataSet from HuggingFace Dataset which is always america.csv
 # download_from_hf_dataset("america.csv", "AmirTrader/TradingViewData", HF_TOKEN_YfOptions)
 DF = load_hf_dataset("america.csv", HF_TOKEN_YfOptions, dataset_name_TradingView_input)
 
 # get ticker list by filtering only above 1 billion dollar company
 # DF = pd.read_csv(f'america_2024-03-01.csv')
-tickerlst  = list(DF.query('`Market Capitalization`>100e9').Ticker)
+tickerlst = list(DF.query("`Market Capitalization`>10e9").Ticker)
 # tickerlist = ['INDO', 'TSLA', 'AAPL', 'MSFT', 'GOOGL', 'AMZN', 'NFLX', 'META', 'NVDA', 'AMD', 'INTC', 'IBM', 'CSCO', 'ORCL', 'QCOM', 'TXN', 'AVGO', 'ADBE', 'CRM', 'NFLX', 'PYPL', 'SNAP']
 
-#measure runtime
+# measure runtime
 start_time = time.time()
 
 option_chain_data_lst = []
 for ticker in tickerlst:
-    if '/' in ticker:
-        pass	
+    if "/" in ticker:
+        pass
     time.sleep(2)  # Pause for 2 seconds between requests
     try:
         # Get the option chain data
@@ -71,16 +71,23 @@ print(option_chain_data_combined.head())
 end_time = time.time()
 print(f"Execution time: {end_time - start_time} seconds")
 # Save the combined option chain data to a CSV file
-option_chain_data_combined.to_csv(os.path.join(outputfolder,'optionchain.csv'), index=False)
+option_chain_data_combined.to_csv(
+    os.path.join(outputfolder, "optionchain.csv"), index=False
+)
 
-file_path = fr'{outputfolder}/optionchain_{current_datetime}.csv'
-latest_file_path = fr'{outputfolder}/optionchain.csv'
+file_path = rf"{outputfolder}/optionchain_{current_datetime}.csv"
+latest_file_path = rf"{outputfolder}/optionchain.csv"
 
-option_chain_data_combined.to_csv(file_path,index=False)
-option_chain_data_combined.to_csv(latest_file_path,index=False)
+option_chain_data_combined.to_csv(file_path, index=False)
+option_chain_data_combined.to_csv(latest_file_path, index=False)
 
 # Upload each file to the dataset
-upload_to_hf_dataset(file_path, dataset_name_YfOptions_output, HF_TOKEN_YfOptions, repo_type="dataset")
-upload_to_hf_dataset(latest_file_path, dataset_name_YfOptions_output, HF_TOKEN_YfOptions, repo_type="dataset")
-
-
+upload_to_hf_dataset(
+    file_path, dataset_name_YfOptions_output, HF_TOKEN_YfOptions, repo_type="dataset"
+)
+upload_to_hf_dataset(
+    latest_file_path,
+    dataset_name_YfOptions_output,
+    HF_TOKEN_YfOptions,
+    repo_type="dataset",
+)
--- yfoptions.py
+++ yfoptions.py
@@ -1,65 +1,84 @@
 import pandas as pd
 import yfinance as yf
 
-#calculating columns how many days to expiration dates
+
+# calculating columns how many days to expiration dates
 def daysleft(inputdate):
-  import datetime
-  previous_date = datetime.datetime.strptime(inputdate, '%Y-%m-%d')
-  today = datetime.datetime.today()
-  ndays = (previous_date  - today).days +1
-  return ndays
+    import datetime
 
-#get All Option Chain for a ticker, calculate additional columns and retun as a dataframe from Yahoo finance
+    previous_date = datetime.datetime.strptime(inputdate, "%Y-%m-%d")
+    today = datetime.datetime.today()
+    ndays = (previous_date - today).days + 1
+    return ndays
+
+
+# get All Option Chain for a ticker, calculate additional columns and retun as a dataframe from Yahoo finance
 def get_DF_optionchain(ticker):
-  import requests
-  session = requests.Session()
-  tkobj = yf.Ticker(ticker , session=session)
-  exps = tkobj.options
+    import requests
 
-  if not exps:
-    print(f"No expiration dates found for the ticker {ticker}")
-    raise ValueError(f"No expiration dates found for the ticker {ticker}.")
-    return None
-    
+    session = requests.Session()
+    tkobj = yf.Ticker(ticker, session=session)
+    exps = tkobj.options
 
-  #Current Price in Market Session
-  lastprice = tkobj.info['currentPrice']
+    if not exps:
+        print(f"No expiration dates found for the ticker {ticker}")
+        raise ValueError(f"No expiration dates found for the ticker {ticker}.")
+        return None
 
-  #base price for additional column
-  # bias_price = 'mark'
-  bias_price = 'lastPrice'
-  print(ticker)
-  optionslst = []
-  for e in exps:
-    try:
-        print(e)
-        opt = tkobj.option_chain(e)
-        # opt_calls = pd.DataFrame().append(opt.calls)
-        opt_calls = opt.calls
-        opt_calls['Type'] = 'CALL'
-        # opt_puts = pd.DataFrame().append(opt.puts)
-        opt_puts = opt.puts
-        opt_puts['Type'] = 'PUT'
-        combined = pd.concat([opt_calls, opt_puts], ignore_index=True)
+    # Current Price in Market Session
+    lastprice = tkobj.info["currentPrice"]
 
-        combined['expirationDate'] = e
-        combined['daysleft'] = daysleft(e)
+    # base price for additional column
+    # bias_price = 'mark'
+    bias_price = "lastPrice"
+    print(ticker)
+    optionslst = []
+    for e in exps:
+        try:
+            print(e)
+            opt = tkobj.option_chain(e)
+            # opt_calls = pd.DataFrame().append(opt.calls)
+            opt_calls = opt.calls
+            opt_calls["Type"] = "CALL"
+            # opt_puts = pd.DataFrame().append(opt.puts)
+            opt_puts = opt.puts
+            opt_puts["Type"] = "PUT"
+            combined = pd.concat([opt_calls, opt_puts], ignore_index=True)
 
-        combined[['bid', 'ask', 'strike']] = combined[['bid', 'ask', 'strike']].apply(pd.to_numeric)
-        combined['mark'] = (combined['bid'] + combined['ask']) / 2
+            combined["expirationDate"] = e
+            combined["daysleft"] = daysleft(e)
 
-        combined['pricepercent'] = 100 * combined[bias_price] / lastprice
-        combined['pricepercentstrike'] = 100 * combined[bias_price] / combined['strike']
-        combined['interinsicvalue'] = combined.apply(lambda row: abs(lastprice - row['strike']) if row['inTheMoney'] else 0, axis=1)
-        combined['interinsicvalue%'] = 100*combined['interinsicvalue'] / combined[bias_price]
-        combined['timevalue'] = combined.apply(lambda row: row[bias_price] - row['interinsicvalue'], axis=1)
-        combined['timevalue%'] = 100*combined['timevalue'] / combined[bias_price]
-        combined['breakevenprice'] = combined.apply(lambda row: row[bias_price]+ row['strike'] if row['Type']=='CALL' else row['strike'] - row[bias_price], axis=1)
-        
-        optionslst.append(combined)
-    
-    except:
-        pass
+            combined[["bid", "ask", "strike"]] = combined[
+                ["bid", "ask", "strike"]
+            ].apply(pd.to_numeric)
+            combined["mark"] = (combined["bid"] + combined["ask"]) / 2
 
-    optionschain = pd.concat(optionslst, ignore_index=True)
-  return optionschain
+            combined["pricepercent"] = 100 * combined[bias_price] / lastprice
+            combined["pricepercentstrike"] = (
+                100 * combined[bias_price] / combined["strike"]
+            )
+            combined["interinsicvalue"] = combined.apply(
+                lambda row: abs(lastprice - row["strike"]) if row["inTheMoney"] else 0,
+                axis=1,
+            )
+            combined["interinsicvalue%"] = (
+                100 * combined["interinsicvalue"] / combined[bias_price]
+            )
+            combined["timevalue"] = combined.apply(
+                lambda row: row[bias_price] - row["interinsicvalue"], axis=1
+            )
+            combined["timevalue%"] = 100 * combined["timevalue"] / combined[bias_price]
+            combined["breakevenprice"] = combined.apply(
+                lambda row: row[bias_price] + row["strike"]
+                if row["Type"] == "CALL"
+                else row["strike"] - row[bias_price],
+                axis=1,
+            )
+
+            optionslst.append(combined)
+
+        except:
+            pass
+
+        optionschain = pd.concat(optionslst, ignore_index=True)
+    return optionschain
